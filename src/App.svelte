11eada19aa2054fab9079ab6076dcd4ac96bad6a
Samy Khatib <khatib.samy@gmail.com>
2022-04-28 16:33:54 +0200

    feat: remove unecessary comments

--- src/App.svelte
+++ src/App.svelte
@@ -4,10 +4,6 @@
   import { ApolloClient, InMemoryCache } from "@apollo/client";
   import { setClient } from "svelte-apollo";
 
-
-
-  // 1. Create an Apollo client and pass it to all child components
-  //    (uses svelte's built-in context)
   const client = new ApolloClient({
     uri: "https://www.hemnet.se/graphql",
 		cache: new InMemoryCache()
